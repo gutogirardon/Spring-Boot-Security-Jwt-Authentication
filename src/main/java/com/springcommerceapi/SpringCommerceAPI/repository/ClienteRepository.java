756688b714df579f258ff231fd2cfe4de000dce3
Gabriela <gabiceemi@gmail.com>
2019-02-14 18:49:47 -0200

    Revertendo a cagada

--- src/main/java/com/springcommerceapi/SpringCommerceAPI/repository/ClienteRepository.java
+++ src/main/java/com/springcommerceapi/SpringCommerceAPI/repository/ClienteRepository.java
@@ -1,5 +1,6 @@
 package com.springcommerceapi.SpringCommerceAPI.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
@@ -12,5 +13,7 @@ import com.springcommerceapi.SpringCommerceAPI.model.Cliente;
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     Cliente findByNome(String nome);
+    Cliente findById(long id);
+    List<Cliente> findAll();    	
 
 }
--- src/main/java/com/springcommerceapi/SpringCommerceAPI/repository/EstoqueRepository.java
+++ src/main/java/com/springcommerceapi/SpringCommerceAPI/repository/EstoqueRepository.java
@@ -3,6 +3,9 @@ package com.springcommerceapi.SpringCommerceAPI.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springcommerceapi.SpringCommerceAPI.model.Estoque;
+import com.springcommerceapi.SpringCommerceAPI.model.Produto;
 
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
+	Estoque findByProdutoId(long id);	
+	Estoque deleteByProdutoId(long id);
 }
